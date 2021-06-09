f8ac0ec3a7263bc6b68ab84af9ad52ef1a6e1fa8
chihyeonOh <74235807+chihyeonOh@users.noreply.github.com>
2021-06-10 01:11:24 +0900

    Add files via upload

---
+++ mysql.php
@@ -0,0 +1,9 @@
+<?php
+    $DB_host = "localhost";
+    $DB_user = "root";
+    $DB_password = "wjsansrk";
+    $DB_name = "Weblog";
+    
+    $connect = mysqli_connect($DB_host,$DB_user,$DB_password,$DB_name);
+
+?>
